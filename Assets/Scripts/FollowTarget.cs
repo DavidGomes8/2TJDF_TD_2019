89f678e24df53e453c97ea869e5b54f63f6733b7
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-30 12:35:33 -0300

    Targets

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -12,15 +12,51 @@ public class FollowTarget : MonoBehaviour {
     public float velocidadeRotacao = 1f;
     public bool lookAt = false;
 
+    public bool buscarSempreMaisProximo;
+
     void Start () {
 		
 	}
 	
 
 	void Update () {
+        Procurar();
         Movimentar();
         Rotacionar();
 	}
+
+    private void Procurar()
+    {
+        if (targetTag==""  || (!buscarSempreMaisProximo && target != null))
+        {
+            return;
+        }
+
+        if (!buscarSempreMaisProximo && target == null)
+        {
+            return;
+        }
+
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
+
+        Transform possivelTarget = null;
+
+        foreach(GameObject checarTarget in targets)
+        {
+            float checarDistancia = Vector3.Distance(checarTarget.transform.position, transform.position);
+
+            if (possivelTarget== null || checarDistancia<Vector3.Distance(possivelTarget.position,transform.position))
+            {
+                possivelTarget = checarTarget.transform;
+            }
+        }
+
+        if(possivelTarget != null)
+        {
+            target = possivelTarget;
+        }
+        
+    }
 
     private void Rotacionar()
     {
